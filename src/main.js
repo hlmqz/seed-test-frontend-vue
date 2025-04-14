936b1afd0dc106c5e5b162c733aa60c9546fc3a7
H Marquez <hlmarquezm@gmail.com>
2025-04-14 12:43:48 -0500

    install:: Instalado lo necesario para librería de componentes Quasar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +5,32 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/es'
+import quasarIconSet from 'quasar/icon-set/material-symbols-outlined'
+
+import { v4 as uuidv4 } from 'uuid';
+
+if(typeof window.crypto.randomUUID == 'undefined'){
+	window.crypto.randomUUID = uuidv4;
+}
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+
+import 'quasar/src/css/index.sass'
+import './assets/main.css'
+
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(Quasar, {
+	plugins: {}, // import Quasar plugins and add here
+	lang: quasarLang,
+	iconSet: quasarIconSet,
+})
 
 app.mount('#app')
