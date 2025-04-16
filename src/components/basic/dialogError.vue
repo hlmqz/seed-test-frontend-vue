456e1f009a6648827d877591ecbf6636ba9e8af3
H Marquez <hlmarquezm@gmail.com>
2025-04-15 19:47:30 -0500

    feat:: mejor visualización de los errores implementando formato por organizeErrors

--- src/components/basic/dialogError.vue
+++ src/components/basic/dialogError.vue
@@ -1,16 +1,26 @@
 <template>
-<q-dialog v-model="show">
-	<q-card class="bg-negative text-white" style="width: auto; max-width: 90vw;">
-		<q-card-section>
+<q-dialog persistent :modelValue="show">
+	<q-card class="text-white" :class="['bg-'+info.color]" style="width: auto; max-width: 90vw;">
+		<q-card-section class="bg-negative">
 			<div class="text-h6">{{ info?.message }}</div>
 		</q-card-section>
-
-		<q-card-section class="q-pt-none">
+		<q-card-section>
+			<b>
+				{{ info.content }}
+			</b>
+			<div v-if="info.errors.length" class="q-py-md text-right">
+			<q-separator class="q-my-sm"/>
+				<div class="text-italic">{{ info.errors.length }} Detalles</div>
+				<q-toggle v-model="showDetail" :label="(showDetail ? 'Ocultar': 'Mostrar' ) + ' detalles'" />
+			</div>
+		</q-card-section>
+		<q-card-section v-if="showDetail" class="q-pt-none">
 			<div>
 			<template v-for="(err, erri) in info.errors" :key="erri">
-				<div class="q-py-sm">
-					<b v-if="!Array.isArray(info.errors)"> {{erri}}: </b>
-					<span> {{err}} </span>
+				<div class="q-py-xm">
+					<b v-if="err.key"> {{ err.key }} : </b>
+					<b v-else> · </b>
+					<span> {{ err.val }} </span>
 				</div>
 			</template>
 			</div>
@@ -30,6 +40,7 @@
 </template>
 <script>
 import serverStore from '@/stores/server';
+import { organizeErrors } from '@/util/helpers';
 
 export default {
 	name: '',
@@ -41,6 +52,7 @@ export default {
 	data(){
 		return {
 			server: serverStore(),
+			showDetail: false,
 		}
 	},
 	watch:{
@@ -55,12 +67,22 @@ export default {
 			return this.server.error?.response?.data;
 		},
 		show(){
+			if(this.server.error) this.showDetail = false;
 			return Boolean(this.server.error);
 		},
 		info(){
-					let status = 0, icon = '', color = '', message = 'Error', errors = [];
+					let icon = '', color = '', message = 'Error';
 
-					status = this.server.error?.status || 100;
+					let content = '';
+
+					if(this.server.error?.data?.message)
+						content = this.server.error.data.message;
+
+					let status = this.server.error?.status || 100;
+					let errors = organizeErrors(this.server.error?.data);
+
+					console.log({ errors });
+
 
 					switch(status){
 
@@ -87,25 +109,28 @@ export default {
 							icon = 'sym_o_electrical_services'
 							color = 'negative';
 							message = 'Ups!';
-							errors = this.server.error.data;
+							break;
+
+						case 419:
+							icon = 'sym_o_electrical_services'
+							color = 'negative';
+							message = 'Algo falló';
 							break;
 
 						case 422:
 							icon = 'sym_o_electrical_services'
 							color = 'negative';
-							message = 'Algo faltó';
-							errors = Object.values(this.server.error.data?.errors).map(ar => ar.join(', '));
+							message = 'Hay que validar';
 							break;
 
 						case 500:
 							icon = 'sym_o_gpp_maybe'
 							color = 'dark';
 							message = 'Error del Sistema';
-							errors = this.server.error.data;
 							break;
 					}
 
-					return { status, icon, color, message, errors };
+					return { status, icon, color, content, message, errors };
 				}
 
 
